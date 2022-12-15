523099e58a47541f671d0c4444aafa071506e4ca
Zenky <bzenky@gmail.com>
2022-12-15 14:15:43 -0300

    style: project card

--- src/components/ProjectCard/styles.ts
+++ src/components/ProjectCard/styles.ts
@@ -6,20 +6,23 @@ export const ProjectCardContainer = styled(Link)`
     height: 190px;
     padding: 16px;
     border-radius: 8px;
+    box-shadow: 0 0 4px rgba(255, 255, 255, 0.4);
     text-decoration: none;
 
-    transition: background 0.2s ease-in-out;
+    transition: all 0.2s ease-in-out;
 
     h3 {
         color: #fff;
     }
 
     p {
+        font-size: 0.875rem;
         color: #B2A7B8;
     }
 
     &:hover {
         background: #3B2A45;
+        box-shadow: 0 0 8px rgba(255, 255, 255, 0.45);
     }
 
     @media only screen and (max-width: 768px) {
