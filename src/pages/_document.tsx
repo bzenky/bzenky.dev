3a23bdaace614cc3a14bc1661f6b8e5e3cf5abd7
Zenky <bzenky@gmail.com>
2022-12-16 10:17:17 -0300

    feat: styled-components ssr

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,48 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-    return (
-        <Html lang='pt-BR'>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta content="Zenky" name="author" />
-                <meta property="og:type" content="website" />
-                <meta content="summary_large_image" name="twitter:card" />
-                <meta name="theme-color" content="#211925" />
-                <meta content="https://bzenky.dev" property="og:url" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-                <link href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@500;600;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />),
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: [initialProps.styles, sheet.getStyleElement()],
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    render() {
+        return (
+            <Html lang='pt-BR'>
+                <Head>
+                    <meta charSet="utf-8" />
+                    <meta content="Zenky" name="author" />
+                    <meta property="og:type" content="website" />
+                    <meta content="summary_large_image" name="twitter:card" />
+                    <meta name="theme-color" content="#211925" />
+                    <meta content="https://bzenky.dev" property="og:url" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                    <link href="https://fonts.googleapis.com/css2?family=Anek+Telugu:wght@500;600;700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
 }
+
