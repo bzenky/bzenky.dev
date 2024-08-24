022a4b714ea84608045b53828b40db4c82a5e426
bzenky <bzenky@gmail.com>
2024-08-23 21:03:30 -0300

    chore: remove pokedex project

--- src/utils/projects-list.ts
+++ src/utils/projects-list.ts
@@ -1,31 +1,28 @@
-import book from '../assets/book.json'
-import clock from '../assets/clock.json'
-import eye from '../assets/eye.json'
-import star from '../assets/star.json'
+import book from "../assets/book.json";
+import clock from "../assets/clock.json";
+import eye from "../assets/eye.json";
+import star from "../assets/star.json";
 
 export const projects = [
   {
     name: "Pomo Pomodoro",
-    description: "Enhance your productivity with this intuitive and appealing Pomodoro timer",
+    description:
+      "Enhance your productivity with this intuitive and appealing Pomodoro timer",
     url: "https://www.pomopomodoro.com",
-    icon: clock
+    icon: clock,
   },
   {
     name: "Todo List",
-    description: "A powerful task management application that gives you complete control over your items.",
+    description:
+      "A powerful task management application that gives you complete control over your items.",
     url: "https://www.todolist.bzenky.dev",
     icon: book,
   },
   {
     name: "Yotei Theme",
-    description: "A dark theme for some apps inspired by a purple/red sky at dawn.",
+    description:
+      "A dark theme for some apps inspired by a purple/red sky at dawn.",
     url: "https://github.com/bzenky/yotei",
     icon: eye,
   },
-  {
-    name: "Pokedex",
-    description: "A simple Pokédex with the first pokémon generation",
-    url: "https://www.pokedex.bzenky.dev/",
-    icon: star,
-  }
-]
+];
