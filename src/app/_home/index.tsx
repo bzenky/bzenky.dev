e19d3e3b08844bad0be57cd32e78039babe44a4b
Zenky <bzenky@gmail.com>
2025-01-05 14:37:13 -0300

    style: home page

--- src/app/_home/index.tsx
+++ src/app/_home/index.tsx
@@ -2,15 +2,15 @@ import { Header } from "../_components/header";
 
 export function HomePage() {
   return (
-    <div className="min-h-screen  flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <div className="flex flex-col items-center justify-items-center sm:p-6 font-[family-name:var(--font-geist-sans)]">
         <Header />
       </div>
       
       <div className="flex flex-col flex-1 h-full justify-center px-30">
-        <h1 className="text-4xl text-violet-500">Bruno Zenky Guimarães Asano</h1>
-        <p className="text-lg mt-8 mb-4">Software Developer</p>
-        <p className="text-lg text-slate-300">Coding ideas and sharing knowledge</p>
+        <h1 className="text-4xl text-violet-500 font-bold">Bruno Zenky Guimarães Asano</h1>
+        <p className="text-lg mt-8 mb-4 text-slate-300">Software Developer</p>
+        <p className="text-lg">Coding ideas and sharing knowledge</p>
       </div>
     </div>
 
