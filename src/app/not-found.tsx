ef906f483f3bed4c2389ddd69c851e25e3e8fa6f
Zenky <bzenky@gmail.com>
2025-01-05 17:38:53 -0300

    feat: add custom 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>404</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-2xl font-bold text-sky-50">404 | Page not found</h1>
+      <p className="mt-4 text-lg text-center">
+        The page you are looking for does not exist or has been moved.
+      </p>
+
+      <Link
+        href="/"
+        className="mt-8 px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 active:bg-blue-700 transition-colors"
+      >
+        Go back to home
+      </Link>
     </div>
   );
 }
