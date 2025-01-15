5e8a1981c3d9ab87440951c576ccaf3b9d8edd60
Zenky <bzenky@gmail.com>
2025-01-15 20:23:08 -0300

    refactor: window size validation

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import { DrawerMenu } from "./DrawerMenu";
 import { LinkMenu } from "./LinkMenu";
 
-
-
 export function Header() {
-  const isSmallScreen = window.innerWidth < 720;
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    setIsSmallScreen(window.innerWidth < 720)
+
+    window.addEventListener('resize', () => {
+      setIsSmallScreen(window.innerWidth < 720)
+    })
+  }, [])
 
   if (isSmallScreen) {
     return <DrawerMenu />;
