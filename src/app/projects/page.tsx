3373b6b298dd5d613fd028039750ba5c665bb521
Zenky <bzenky@gmail.com>
2025-02-26 07:19:21 -0300

    chore: add title attribute to the project title

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -26,6 +26,7 @@ export default function Projects() {
           href="https://github.com/bzenky/yotei" 
           target="_blank"
           className="group"
+          title="Open Yotei repository"
         >
           <h2 className="text-2xl font-bold mt-6 mb-4 text-slate-300 font-[family-name:var(--font-spectral)] group-hover:text-violet-500 transition-all">
             Yotei
@@ -49,18 +50,19 @@ export default function Projects() {
           href="https://www.pomopomodoro.com/" 
           target="_blank"
           className="group"
+          title="Open Pomo Pomodoro website"
         >
           <h2 className="text-2xl font-bold mt-6 mb-4 text-slate-300 font-[family-name:var(--font-spectral)] group-hover:text-red-500 transition-all">
-            Pomo-Pomodoro
+            Pomo Pomodoro
           </h2>
         </a>
 
         <div className="flex flex-col gap-2">
           <p>
-            Pomo-Pomodoro is a productivity tool that can help you to focus on your tasks and improve your productivity. The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks.
+            Pomo Pomodoro is a productivity tool that can help you to focus on your tasks and improve your productivity. The Pomodoro Technique is a time management method developed by Francesco Cirillo in the late 1980s. The technique uses a timer to break down work into intervals, traditionally 25 minutes in length, separated by short breaks.
           </p>
           <p>
-            This app was originally created mixing the Pomodoro with youtube lo-fi live streams, as a way to help me focus on my tasks. Now the app is in v2, it was rewritten with the same purpose as this portfolio, to use a new version of the Next.js(14 at time) framework and to improve the codebase.
+            This app was originally created mixing the Pomodoro with youtube lo-fi live streams, as a way to help me focus on my tasks. Now the app is in v2, it was rewritten with the same purpose as this portfolio, to use a new version of the Next.js (14 at time) framework and to improve the codebase.
           </p>
           <p>
             The app is available for free and can be used by anyone who wants to improve their productivity.
